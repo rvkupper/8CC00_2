cd7a20cadc73097e66784aeedab4e635cc535674
Rebecca <rvkupper@gmail.com>
2021-04-14 23:25:42 +0200

    Added silhouette function, appended some more doccstring info

--- clusteringAndClassificationAssignment/clustering.py
+++ clusteringAndClassificationAssignment/clustering.py
@@ -1,10 +1,14 @@
-"""Python scipt for clustering of data by means of k-means and ...
+"""Python scipt for clustering of data by means of k-means and evaluation with silhouette scores.
 """
 
 import random
 
 def squaredEuclideanDist(u, v) -> float:
     """Calculate the Euclidean squared distance between u and v.
+    
+    :param u: 1D or ND coordinate in int or float, or list or tuple respecitvely.
+    :param v: 1D or ND coordinate in int or float, or list or tuple respecitvely.
+    :returns: float of Euclidean squared distance between u and v.
     
     """
     if isinstance(u, float) or isinstance(u, int):
@@ -29,9 +33,10 @@ def squaredEuclideanDist(u, v) -> float:
     return dist
     
 def calculateCentroids(clusteredData: list, dim: int) -> list:
-    """Recalculate the new centroid based on the old cluster configuration.
+    """Recalculate the new centroid based on the averages in the old cluster configuration.
     
     :param dim: required dimensions for the centroids.
+    :returns: List containing new centroids on the position of the average of the cluster.
     """
     new_centroids = []
     # Check dimensions
@@ -126,10 +131,76 @@ def kMeans(data: list, k: int, distMethod: str, maxit: int) -> set:
             old_clusters = clusters
         
     
-    return clusters
+    return clusters, centroids
+    
         
+def silhouetteScore(clusteredData: list) -> tuple:
+    """Calculate silhouette score for clustered data. Function returns tuple containing on the first index the silhouette score for the clustering and in the second index a dict containing the silhouettes for all datapoints.
+    
+    :param clusteredData: list containing sets of data per cluster 
+    :returns: (float, dict) where the float is the overall silhouette score for the clustering and the dict contains the silhouette per datapoint like {(coordinate): silhouette}
+    
+    :examples:
+    >>> v = [{(1.5, 0.5), (1., 1.5), (0.5, 0.5), (0.5, 2.)}, {(6, 6), (5.5, 6), (6, 5.5)}, {(4.5, 2.), (4., 2.), (3.5, 1.5)}]
+    >>> x, y = silhouetteScore(v)
+    0.9352832294102621 {(1.0, 1.5): 0.8928571428571429, (0.5, 0.5): 0.924812030075188, (1.5, 0.5): 0.8567493112947658, (0.5, 2.0): 0.8986666666666667, (6, 6): 0.9884169884169884, (6, 5.5): 0.9858490566037735, (5.5, 6): 0.9873949579831933, (4.5, 2.0): 0.9482758620689655, (3.5, 1.5): 0.9147540983606557, (4.0, 2.0): 0.9550561797752809}
+    """
+    meanWithinClusters = {}
+    meanOtherClusters = {}
+    silhouettes = {}
+    for cluster in clusteredData:
+        datapoints = list(cluster)
+        
+        # Create list for the clusters that active_point is not part of
+        otherClusters = clusteredData.copy()
+        otherClusters.remove(cluster)
+        
+        distInCluster = []
+        for active_point in datapoints:
+            
+            # Mean distance within it's own cluster 
+            for point in datapoints:
+                d = squaredEuclideanDist(active_point, point)
+                distInCluster.append(d)
+            meanDist = sum(distInCluster)/(len(distInCluster) - 1)
+            meanWithinClusters[active_point] = meanDist
+            
+            # Mean distance to all points from the nearest cluster 
+            for otherCluster in otherClusters:
+                distOtherCluster = []
+                
+                for otherPoint in list(otherCluster):
+                    s = squaredEuclideanDist(active_point, otherPoint)
+                    distOtherCluster.append(s)
+                
+                meanOtherDist = sum(distOtherCluster)/len(distOtherCluster)
+                                
+                if active_point in meanOtherClusters:
+                    if meanOtherClusters[active_point] > meanOtherDist:
+                        meanOtherClusters[active_point] = meanOtherDist                                                
+                else:
+                    meanOtherClusters[active_point] = meanOtherDist
+            
+            # Calculate silhouette for each datapoint        
+            a = meanDist 
+            b = meanOtherClusters[active_point]
+            
+            silhouette = (b - a) / max(a, b)
+            silhouettes[active_point] = silhouette
+    
+    # Calculate overall silhouette score
+    silhouetteScore = sum(silhouettes.values())/len(silhouettes)                
+    return silhouetteScore, silhouettes 
+            
+    
+    
+    
+    
+    
+    
     
 l = [(1.2, 1.5), (0.6, 0.5), (0.5, 1.7), (1.5, 0.5), (6, 6), (5.7, 6), (6, 5.4)]
-print(kMeans(l, 2, 'a', 10))
-v = [{(1.5, 0.5), (1.2, 1.5), (0.6, 0.5), (0.5, 1.7)}, {(6, 6), (5.7, 6), (6, 5.4)}]
-# print(calculateCentroids(v, 2))        
+# print(kMeans(l, 2, 'a', 10))
+v = [{(1.5, 0.5), (1., 1.5), (0.5, 0.5), (0.5, 2.)}, {(6, 6), (5.5, 6), (6, 5.5)}, {(4.5, 2.), (4., 2.), (3.5, 1.5)}]
+x, y = silhouetteScore(v)
+print(x, y)
