734718b0f635f568b8f8a6491a85d0c512b6b9a9
Rebecca <rvkupper@gmail.com>
2021-04-16 15:09:17 +0200

    Added k-means clustering and k-nearest neighbours classification

--- clusteringAndClassificationAssignment/main.py
+++ clusteringAndClassificationAssignment/main.py
@@ -3,9 +3,11 @@
 
 import matplotlib.pyplot as plt 
 import clustering
+import classification
 import dataETL
 from numpy import arange
 import HCS 
+from collections import Counter
 
 
 if __name__ == '__main__':
@@ -14,33 +16,43 @@ if __name__ == '__main__':
     data_unselected, celllinenames_unselected, genes = dataETL.extractData("data/GDSC_RNA_expression.csv")
     data = dataETL.selectData(data_unselected)
     celllinenames = dataETL.selectData(celllinenames_unselected)
-    
+    labels = dataETL.extractLabels('data/GDSC_metadata.csv')
+
     ### Clustering 
     
     ## k-means and silhouette analysis
     
+    print("k silhouette score")
+    scores = []
+    for k in range(2, 9):
+        scorePerK = []
+        for i in range (20):
+            clusters = clustering.kMeans(data, k, 'sqeu', 1000)
+            totalSilhouette, silhouettes = clustering.silhouetteScore(clusters)
+            scorePerK.append(totalSilhouette)
+        score = max(scorePerK)
+        scores.append(max(scorePerK))
+        print(k, score)
+    
+    
+    
     
     ## Highly connected subgraph clustering
+    
     corrCoef = clustering.overallCorrelationcoefficients(data, celllinenames)
-    # print(len(corrCoef))
-    # print(list(corrCoef.keys())[0])
     thresholds = arange(0, 1.01, 0.01)
     f = []
     for c in thresholds:
         f.append(clustering.nodepairFraction(corrCoef, c))
     
     # plot
-    # plt.plot(thresholds, f)
-    # plt.xlabel('c')
-    # plt.ylabel('f(c)')
-    # plt.grid()
-    # plt.show()
-    # f(c) ongeveer 0.1 voor c = 0.66
+    plt.plot(thresholds, f)
+    plt.xlabel('c')
+    plt.ylabel('f(c)')
+    plt.grid()
+    plt.show() # f(c) ongeveer 0.1 voor c = 0.66
     
     edges = clustering.createEdges(0.66, corrCoef)
-    # edgesTest = [('a', 'b'), ('a', 'c'), ('a', 'd'), ('b', 'd'), ('c', 'd'), ('c', 'e'), ('d', 'e')]
-
-    # graph1 = HCS.Graph(edges)
     
     nrIt = 50
     cuts = []
@@ -52,13 +64,33 @@ if __name__ == '__main__':
             cuts.append(nrCuts)
             nrIt -= 1
         maxIt -= 1
-
+    
     minCuts = min(cuts)
     print(cuts)
     print(minCuts)
     
     
+    ### Classification 
+    errorScore = Counter()
     
-    
-    
+    for i in range(len(data)):
+        
+        # Generate training dataset and point of interest 
+        trainingset, trainingnames, point_of_interest = classification.generateTrainingset(data, i, celllinenames)
+        datapoint = point_of_interest[1]
+        datapointname = point_of_interest[0][0]
+        
+        # Perfrom k-nearest neighbours
+        for k in range(1, 10):
+            found_label = classification.nearestNeighbour(trainingset, datapoint, k, trainingnames, labels)
+
+                    
+            # Check found label with actual label 
+            if not classification.checkLabel(datapointname, found_label, labels):
+                title = "k = "+str(k)
+                errorScore[title] += 1
+            
+            
+    print(errorScore)        
+            
     
